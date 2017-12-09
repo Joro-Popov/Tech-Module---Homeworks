ea71a3a555f2cf2e4f8ce72023980a3da9f3f7e6
Georgi Popov <popov937@abv.bg>
2017-12-09 14:22:41 +0200

    Add homework

---
+++ Software Technologies/Exam preparation IMDB/C# Skeleton/IMDB/Controllers/FilmController.cs
@@ -0,0 +1,130 @@
+﻿using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using IMDB.Models;
+
+namespace IMDB.Controllers
+{
+    [ValidateInput(false)]
+    public class FilmController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            using (var database = new IMDBDbContext())
+            {
+                var films = database.Films.ToList();
+                return View(films);
+            }
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Film film)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var database = new IMDBDbContext())
+                {
+
+                    database.Films.Add(film);
+                    database.SaveChanges();
+                    return Redirect("/");
+                }
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public ActionResult Edit(int? id)
+        {
+            using (var database = new IMDBDbContext())
+            {
+                var film = database.Films.Find(id);
+
+                if(film != null)
+                {
+                    return View(film);
+                }
+            }
+
+            return Redirect("/");
+        }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditConfirm(int? id, Film filmModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(filmModel);
+            }
+
+            using (var database = new IMDBDbContext())
+            {
+                var film = database.Films.Find(filmModel.Id);
+
+                if (film != null)
+                {
+                    film.Name = filmModel.Name;
+                    film.Genre = filmModel.Genre;
+                    film.Director = filmModel.Director;
+                    film.Year = filmModel.Year;
+
+                    database.SaveChanges();
+                }
+
+            }
+
+            return Redirect("/");
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public ActionResult Delete(int? id)
+        {
+            using (var database = new IMDBDbContext())
+            {
+                var film = database.Films.Find(id);
+
+                if (film != null)
+                {
+                    return View(film);
+                }
+            }
+
+            return Redirect("/");
+        }
+
+        [HttpPost]
+        [Route("delete/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int? id, Film filmModel)
+        {
+            using (var database = new IMDBDbContext())
+            {
+                var film = database.Films.Find(id);
+
+                if (film != null)
+                {
+                    database.Films.Remove(film);
+                    database.SaveChanges();
+                }
+            }
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ Software Technologies/Exam preparation IMDB/JavaScript Skeleton/controllers/film.js
@@ -0,0 +1,64 @@
+const Film = require('../models/Film');
+
+module.exports = {
+	index: (req, res) => {
+        Film.find().then(film => {
+
+        	res.render('film/index', {'films': film})
+		});
+	},
+	createGet: (req, res) => {
+        res.render('film/create');
+	},
+	createPost: (req, res) => {
+
+		let film = req.body;
+		Film.create(film).then(film => {
+			res.redirect("/");
+		}).catch(err => {
+			film.error = "Cannot create film.";
+			res.render('task/create, task');
+		});
+	},
+	editGet: (req, res) => {
+        let filmId = req.params.id;
+        Film.findById(filmId).then(film => {
+        	if(film){
+        		res.render('film/edit', film);
+			}else{
+
+        		res.redirect('/');
+			}
+		})
+	},
+	editPost: (req, res) => {
+        let filmId = req.params.id;
+        let film = req.body;
+
+        Film.findByIdAndUpdate(filmId, film, {runValidators: true}).then(film => {
+        	res.redirect("/");
+		}).catch(err => {
+			film.id = filmId;
+			film.error = "Cannot edit film.";
+			return res.render("film/edit", film);
+		})
+	},
+	deleteGet: (req, res) => {
+        let filmId = req.params.id;
+        Film.findById(filmId).then(film => {
+        	if(film){
+
+        		return res.render('film/delete', film);
+			}else{
+
+        		return res.redirect('/');
+			}
+		})
+	},
+	deletePost: (req, res) => {
+        let filmId = req.params.id;
+        Film.findByIdAndRemove(filmId).then(film => {
+        	res.redirect('/');
+		}).catch(err => res.redirect('/'));
+	}
+};
